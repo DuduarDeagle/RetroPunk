65559813fd895d74a776fabfedd87c610144289b
Duduar <support@duduar.dev>
2025-01-07 16:16:46 -0600

    💄 Add a button component

---
+++ components/Buttons/Button.stories.tsx
@@ -0,0 +1,97 @@
+/* Import types */
+import * as React from "react"
+import type { Meta, StoryObj } from "@storybook/react"
+
+/* Import components */
+import Button from "./Button"
+
+/* Define types */
+type Story = StoryObj<typeof Button>
+
+/* Define meta */
+export default {
+	title: "Atoms/Button",
+	component: Button,
+	args: {
+		label: "Click me"
+	},
+	tags: ["autodocs"]
+} satisfies Meta<typeof Button>
+
+/* Set up stories */
+export const Default: Story = {}
+
+export const Transparent: Story = {
+	args: {
+		mode: "transparent"
+	}
+}
+
+export const CallToAction: Story = {
+	args: {
+		mode: "cta",
+		label: "Click me now!"
+	}
+}
+
+export const WithIcon: Story = {
+	args: {
+		label: "Play"
+	},
+	render: args => (
+		<Button {...args}>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 24 24"
+				fill="currentColor">
+				<path d="M8 5.14v14l11-7z" />
+			</svg>
+		</Button>
+	)
+}
+
+export const WithIconAtRight: Story = {
+	args: {
+		label: "Play",
+		reverse: true
+	},
+	render: args => (
+		<Button {...args}>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 24 24"
+				fill="currentColor">
+				<path d="M8 5.14v14l11-7z" />
+			</svg>
+		</Button>
+	)
+}
+
+export const IconOnly: Story = {
+	args: {
+		label: undefined
+	},
+	render: args => (
+		<Button {...args}>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 24 24"
+				fill="currentColor">
+				<path d="M8 5.14v14l11-7z" />
+			</svg>
+		</Button>
+	)
+}
+
+export const Danger: Story = {
+	args: {
+		mode: "danger"
+	}
+}
+
+export const Disabled: Story = {
+	args: {
+		disabled: true,
+		label: "Can't click me"
+	}
+}
---
+++ components/Buttons/Button.tsx
@@ -0,0 +1,62 @@
+/* Import dependencies */
+import * as React from "react"
+import clsx from "clsx"
+
+/* Import styles */
+import "./Button.css"
+
+/* Define interfaces */
+interface Props
+	extends React.DetailedHTMLProps<
+		React.ButtonHTMLAttributes<HTMLButtonElement>,
+		HTMLButtonElement
+	> {
+	/**
+	 * What is the button style?
+	 */
+	mode?: "transparent" | "cta" | "danger"
+	/**
+	 * Text to be displayed on the button
+	 */
+	label?: string
+	/**
+	 * Specifies the icon position. If true, the icon is positioned at the end
+	 */
+	reverse?: boolean
+}
+
+/* Set up a button component */
+const Button: React.FC<Props> = ({
+	mode,
+	label,
+	reverse,
+	children,
+	...props
+}) => {
+	return (
+		<button
+			className={clsx(
+				"btn",
+				!label && "btn--icon",
+				mode === "transparent" && "btn--transparent",
+				mode === "cta" && "btn--cta",
+				mode === "danger" && "btn--danger"
+			)}
+			{...props}>
+			{!reverse &&
+				React.Children.count(children) <= 1 &&
+				React.isValidElement(children) &&
+				children.type === "svg" &&
+				children}
+			{label && <span className="label">{label}</span>}
+			{reverse &&
+				React.Children.count(children) <= 1 &&
+				React.isValidElement(children) &&
+				children.type === "svg" &&
+				children}
+		</button>
+	)
+}
+
+/* Export the button component by default */
+export default Button
